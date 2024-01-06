b9d6424dc59e345209dbca80a9b656952e2e343e
Chiến <chieng6600@gmail.com>
2024-01-06 21:29:03 +0700

    done all logic game 2

--- app/src/main/java/com/groups/BrainTrainApp/Memory/Memory_2.kt
+++ app/src/main/java/com/groups/BrainTrainApp/Memory/Memory_2.kt
@@ -3,6 +3,8 @@ package com.groups.BrainTrainApp.Memory
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.ViewGroup
 import android.widget.Button
@@ -11,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.groups.BrainTrainApp.Components.Common.ButtonCustom
 import com.groups.BrainTrainApp.MainActivity
 import com.groups.BrainTrainApp.R
+import com.groups.BrainTrainApp.Utils.borderView
 import com.groups.BrainTrainApp.Utils.drawButton
 
 class Memory_2 : AppCompatActivity() {
@@ -21,6 +24,7 @@ class Memory_2 : AppCompatActivity() {
     var numCol = 2
     var playercount = 0
     var lvFlag: Boolean = false
+    private var handler: Handler = Handler(Looper.getMainLooper())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.memory_game_1)
@@ -28,13 +32,11 @@ class Memory_2 : AppCompatActivity() {
         btnBack.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
-
         totalLayout = findViewById(R.id.totalLayout)
         addButton()
         addButton()
         addButton()
         addButton()
-
         nextLv()
     }
 
@@ -59,41 +61,65 @@ class Memory_2 : AppCompatActivity() {
     private fun chosenButton(clickedButton: ButtonCustom) {
         if (clickedButton.isChoose) {
             clickedButton.isChoose = false
+            clickedButton.isEnabled = false
             clickedButton.setBackgroundColor(Color.BLUE)
             playercount++
             if (playercount == count) {
+                disableAllButton()
                 playercount = 0
                 count++
-                clearColor()
-                nextLv()
+                handler.postDelayed({
+                    clearColor()
+                    nextLv()
+                }, 2000)
             }
         } else {
             clickedButton.setBackgroundColor(Color.RED)
-            Log.d("lose", "u lose")
+            appearColor()
+            disableAllButton()
         }
-
     }
 
     private fun clearColor() {
         for (i in buttonList.indices) {
             val button = buttonList[i]
             button.setBackgroundColor(Color.WHITE)
+            button.isEnabled = true
+        }
+    }
+
+    private fun appearColor() {
+        for (i in buttonList.indices) {
+            val button = buttonList[i]
+            if(button.isChoose)
+                button.setBackgroundColor(Color.BLUE)
+        }
+    }
+
+    private fun disableAllButton() {
+        for (i in buttonList.indices) {
+            val button = buttonList[i]
+            button.isEnabled = false
         }
     }
 
     private fun nextLv() {
         for (i in 0 until count) {
             buttonList.get(i).isChoose = true
+            buttonList.get(i).setBackgroundColor(Color.BLUE)
         }
+        for (i in 0 until numCol) {
+            addButton()
+        }
+        disableAllButton()
         if (!lvFlag) {
             lvFlag = true
             numCol++
         } else {
             lvFlag = false
-
         }
-        for (i in 0 until count) {
-            addButton()
-        }
+        handler.postDelayed({
+            clearColor()
+        }, 3000)
     }
 }
